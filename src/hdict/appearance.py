eac618ba0abfa4713467bb118c06653f43018ddf
davi <dpsabc@gmail.com>
2023-01-28 21:26:30 -0300

    Add copyright notice

--- src/hdict/appearance.py
+++ src/hdict/appearance.py
@@ -1,19 +1,19 @@
 #  Copyright (c) 2021. Davi Pereira dos Santos
-#  This file is part of the idict project.
+#  This file is part of the hdict project.
 #  Please respect the license - more about this in the section (*) below.
 #
-#  idict is free software: you can redistribute it and/or modify
+#  hdict is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  idict is distributed in the hope that it will be useful,
+#  hdict is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with idict.  If not, see <http://www.gnu.org/licenses/>.
+#  along with hdict.  If not, see <http://www.gnu.org/licenses/>.
 #
 #  (*) Removing authorship by any means, e.g. by distribution of derived
 #  works or verbatim, obfuscated, compiled or rewritten versions of any
